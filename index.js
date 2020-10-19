70c248ffd4b3c327c4a9336fa63904fca1e4acb3
Andre <itstheandre@gmail.com>
2020-10-19 11:48:44 +0200

    Variables

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+console.log("LIVE FROM THE CLASS");
+// let; const''
+
+// variable declaration
+let ordinaryNumber = 1;
+
+// variable reassignment
+ordinaryNumber = "2";
+
 const hello = "world";
+// hello = "something else" ❌
+
+let bananas;
+// const apples -> big no no
+
+let fuits;
+let vegetables;
