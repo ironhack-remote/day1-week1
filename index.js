f1d9c9a2cc2d751fb9cc34405941629d6e473eb5
Andre <itstheandre@gmail.com>
2020-10-19 12:01:54 +0200

    Adding comments for bad usecases

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ console.log(typeof ordinaryNumber);
 ordinaryNumber = "2";
 
 const hello = "world";
-// hello = "something else" ❌
+// hello = "something else" ❌ -> We will have an error. Trying to reassign a const. We cant do this
 
 let bananas;
-// const apples -> big no no
+// const apples -> big no no. bananas can be undefined from the get go. But if we want apples to be undefined we have to be explicit
+
+// let fruits, vegetables; ❌ -> Dont recommend because its not the most widely used naming structure;
 
 let fuits;
 let vegetables;
@@ -20,8 +22,9 @@ let vegetables;
 console.clear();
 
 console.log(typeof ordinaryNumber);
-// const 1Two = "cool beans" ❌
+// const 1Two = "cool beans" ❌ -> variables MUST start with a letter
 
+// DATA TYPES
 // array, object, number, string, boolean, null, undefined, NaN,symbol
 // boolean true | false
 
